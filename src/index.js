ee8e11e4930861f8372be2cf9d2ae8484ee4111b
GemsGame <support@gems.cash>
2018-09-20 17:52:21 +0300

    13\12

--- src/index.js
+++ src/index.js
@@ -23,16 +23,96 @@ class Sorter {constructor() {
   toArray() {
     return this.array;
   }
-        //[0]
-  sort(indices) {
-    // your implementation
-       //sorter.sort([0])
-      return this.array[indices];
+       
+sort(indices) {
+  
+//сделать новый массив и пихнуть в this.array
+  
+
+//var arr1 = [5,8,2,7]; //значения
+//var arr2 = [1,2]; //индексы
+    
+var arr1 = this.array;
+var arr2 = indices;   
+    
+var arrSort = []; // сортировка
+
+
+var arrNewValue = []; // переставленные значения
+
+
+for (var i = 0; i < arr2.length; i++) {        
+   
+        //console.log("индекс:" + arr2[i]);
+        //console.log("значение:" + arr1[arr2[i]]);
+        arrSort.push(arr1[arr2[i]]);
+     
       
+
+       
+}
+
+
+
+arrSort.sort(compareNumeric);
+
+for (var i = 0; i < arrSort.length; i++) {        
+        //console.log("значение: " + arrSort[i]);
+    
+         arrNewValue.push(arrSort[i]);
+}
+
+
+///////////////////////////////////////////////////////////// функция cортиратор!
+function compareNumeric(a, b) {
+  if (a > b) return 1;
+  if (a < b) return -1;
+}
+
+
+
+
+
+////////////////////////////////////////////////////////////////функция кромсатор и перезаписатор
+
+  /*  for (var i = 0; i < arrNewValue.length; i++)
+      console.log(arrNewValue[i]);  
+    
+    
+    for (var i2 = 0; i2 < arr2.length; i2++) 
+      console.log(arr2[i2]);  
+    */
+    
+    
+    
+
+function cuts (arrNewValue, arr2) {
+
+
+for (var i = 0; i < arrNewValue.length; i++) {
+     //console.log(arrNewValue[i]);
+     //console.log(arr2[i]);
+     var removed = arr1.splice(arr2[i], 1, arrNewValue[i]);
+    
+    
+}
+    
+  return arr1;     
+
+}
+cuts(arrNewValue,arr2);
+//console.log(arr1);
+//console.log(arrNewValue);
+
+
+//return this.array = [5, 6, 7];
+    
   }
 
   setComparator(compareFunction) {
     // your implementation
+
+      
   }
 }
 
